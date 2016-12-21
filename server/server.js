8850e5e9c931ee7297056b933c3ac665941159ca
Collin Tharp <catharp@gmail.com>
2016-12-21 11:06:10 -0800

    [MODIFY](server): add body parser and path {CT}

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
+// configure server
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
 var port = 3000;
+var app = express();
 
-app.use(express.static('../client'));
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// serve static files
+app.use(express.static(path.join(__dirname, '../client')));
 
+// start server
 app.listen(3000, () => console.log("Listening on port", port));
