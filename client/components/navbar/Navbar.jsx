80f7543232869764f259c652049b1e73e2c908fd
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-21 12:28:35 -0800

    [MODIFY](Server): Use webpack dev middleware to compile code {VE}

--- client/components/navbar/Navbar.jsx
+++ client/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Test () => {
+const Test = () => {
   return <p>This is a test</p>
 }
 
--- server/server.js
+++ server/server.js
@@ -4,14 +4,24 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var webpack = require('webpack');
 var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpackConfig = require('../webpack.config.js')
 
 
 var port = 3000;
 var app = express();
 
+
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var compiler = webpack(webpackConfig);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: "/"
+}));
+
+
 // serve static files
 app.use(express.static(path.join(__dirname, '../dist')));
 
