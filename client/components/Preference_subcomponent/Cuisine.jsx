e740503e4f78489ee40b7f9134d8235b84c4f2c0
zeninspire <zeninspire@gmail.com>
2016-12-23 11:01:24 -0800

    [INIT](Client): Added Cuisine in components/preference_subcomponents folder. {AZ}

---
+++ client/components/Preference_subcomponent/Cuisine.jsx
@@ -0,0 +1,36 @@
+import React, {Component, PropTypes} from 'react'
+import { SplitButton, MenuItem } from 'react-bootstrap';
+
+class Cuisine extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      selectedCuisine: 'Cuisine'
+    };
+    this.updateCuisine=this.updateCuisine.bind(this);
+
+  }
+
+  updateCuisine(name) {
+    this.setState({selectedCuisine: name})
+  }
+
+
+  render () {
+    return (
+      <div>This is the Preference_Cuisine Container! <br />
+
+        <SplitButton bsStyle='info' title={this.state.selectedCuisine} id={'split-button-basic-0'}>
+          <MenuItem onSelect={this.updateCuisine} eventKey="Chinese">Chinese</MenuItem>
+          <MenuItem onSelect={this.updateCuisine} eventKey="Italian">Italian</MenuItem>
+          <MenuItem onSelect={this.updateCuisine} eventKey="American">American</MenuItem>
+          <MenuItem onSelect={this.updateCuisine} eventKey="Pho">Pho</MenuItem>
+        </SplitButton><br /><br />
+
+      </div>
+    )
+  }
+}
+
+export default Cuisine
