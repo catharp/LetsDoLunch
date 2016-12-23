0b4240b2e5ab60ff4fc687ed8336f5d4e7fde505
zeninspire <zeninspire@gmail.com>
2016-12-23 10:48:30 -0800

    [MODIFY](Client): Updated Preference container and added react-bootstrap in package.json

---
+++ client/containers/Preference.jsx
@@ -0,0 +1,22 @@
+import React, {Component, PropTypes} from 'react'
+//import {Router, browswerHistory} from 'react-router'
+//import {connect, Provider} from 'react-redux'
+
+class PreferenceContainer extends Component {
+  render () {
+    return (
+      <div>This is the Preference Container and this is a test!</div>
+    )
+  }
+}
+
+
+// function mapStateToProps(state) {
+//   return {
+//     //TODO
+//   }
+// }
+
+// export default connect(mapStateToProps)(PreferenceContainer);
+
+export default PreferenceContainer
