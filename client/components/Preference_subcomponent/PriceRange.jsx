9ad6a3d3f94cdf036ae5e9f657e515f6370bad0b
zeninspire <zeninspire@gmail.com>
2016-12-23 11:02:25 -0800

    [INIT](Client): Added PriceRange component in components/preference_subcomponents folder. {AZ}

---
+++ client/components/Preference_subcomponent/PriceRange.jsx
@@ -0,0 +1,35 @@
+import React, {Component, PropTypes} from 'react'
+import { SplitButton, MenuItem } from 'react-bootstrap';
+
+class PriceRange extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      selectedPriceRange: 'Price Range'
+    };
+    this.updatePriceRange=this.updatePriceRange.bind(this);
+
+  }
+
+  updatePriceRange(name) {
+    this.setState({selectedPriceRange: name})
+  }
+
+
+  render () {
+    return (
+      <div>This is the Preference_PriceRange Container! <br />
+
+        <SplitButton bsStyle='info' title={this.state.selectedPriceRange} id={'split-button-basic-0'}>
+          <MenuItem onSelect={this.updatePriceRange} eventKey="$">$</MenuItem>
+          <MenuItem onSelect={this.updatePriceRange} eventKey="$$">$$</MenuItem>
+          <MenuItem onSelect={this.updatePriceRange} eventKey="$$$">$$$</MenuItem>
+          <MenuItem onSelect={this.updatePriceRange} eventKey="$$$$">$$$$</MenuItem>
+        </SplitButton><br /><br />
+      </div>
+    )
+  }
+}
+
+export default PriceRange
