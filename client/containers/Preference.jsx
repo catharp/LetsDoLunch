28c76349a02d73cbb956bb3799b7b9d1384bc82f
zeninspire <zeninspire@gmail.com>
2016-12-23 10:47:16 -0800

    [MODIFY](Client): Updated Preference container and added react-bootstrap in package.json

--- client/containers/Preference.jsx
+++ client/containers/Preference.jsx
@@ -1,14 +1,19 @@
 import React, {Component, PropTypes} from 'react'
 import { SplitButton, MenuItem } from 'react-bootstrap';
+import Time from '../components/Preference_subcomponent/Time.jsx';
+import Cuisine from '../components/Preference_subcomponent/Cuisine.jsx';
+import PriceRange from '../components/Preference_subcomponent/PriceRange.jsx';
+import Neighborhood from '../components/Preference_subcomponent/Neighborhood.jsx';
+
 //import {Router, browswerHistory} from 'react-router'
 //import {connect, Provider} from 'react-redux'
 
-class PreferenceContainer extends Component {
+class Preference extends Component {
 
   constructor(props) {
     super(props);
     this.state={
-      selectedCuisine: 'Cuisine2'
+      selectedCuisine: 'Cuisine'
     };
     this.updateCuisine=this.updateCuisine.bind(this);
 
@@ -23,31 +28,11 @@ class PreferenceContainer extends Component {
     return (
       <div>This is the Preference Container and this is a test! <br />
 
-        <SplitButton bsStyle='info' title={this.state.selectedCuisine} id={'split-button-basic-0'}>
-          <MenuItem onSelect={this.updateCuisine} eventKey="Chinese">Chinese</MenuItem>
-          <MenuItem onSelect={this.updateCuisine} eventKey="Italian">Italian</MenuItem>
-          <MenuItem onSelect={this.updateCuisine} eventKey="American">American</MenuItem>
-          <MenuItem onSelect={this.updateCuisine} eventKey="Pho">Pho</MenuItem>
-        </SplitButton><br /><br />
+        <Cuisine />
+        <PriceRange />
+        <Neighborhood />
+        <Time />
 
-        <SplitButton bsStyle='info' title={'Price Range'} id={'split-button-basic-0'}>
-          <MenuItem eventKey="1">$</MenuItem>
-          <MenuItem eventKey="2">$$</MenuItem>
-          <MenuItem eventKey="3">$$$</MenuItem>
-          <MenuItem eventKey="4">$$$$</MenuItem>
-        </SplitButton><br /><br />
-
-        <SplitButton bsStyle='info' title={'Neighborhood'} id={'split-button-basic-0'}>
-          <MenuItem eventKey="1">SOMA</MenuItem>
-          <MenuItem eventKey="2">FiDi</MenuItem>
-          <MenuItem eventKey="3">Mission</MenuItem>
-          <MenuItem eventKey="4">Tenderloin</MenuItem>
-        </SplitButton><br /><br />
-
-        <SplitButton bsStyle='info' title={'Time'} id={'split-button-basic-0'}>
-          <MenuItem eventKey="1">Now</MenuItem>
-          <MenuItem eventKey="2">Later</MenuItem>
-        </SplitButton><br /><br />
       </div>
     )
   }
@@ -63,4 +48,4 @@ class PreferenceContainer extends Component {
 
 // export default connect(mapStateToProps)(PreferenceContainer);
 
-export default PreferenceContainer
+export default Preference
