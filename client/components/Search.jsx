dc647bc185824c4b4e53ec3d99b094e8f9110984
Collin Tharp <catharp@gmail.com>
2016-12-28 11:31:01 -0800

    [MODIFY](client): get map markers working and clean up map component {CT}

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -3,19 +3,10 @@ import Preference from '../containers/Preference.jsx';
 import Map from '../containers/Map.jsx';
 
 export default () => {
-  const markers = [
-    {
-      location: {
-        lat: 37.7749,
-        lng: -122.4194
-      }
-    }
-  ]
-
   return (
     <div>
       <div>
-        <Map center={location} markers={markers} />
+        <Map />
       </div>
       <Preference />
     </div>
--- client/containers/Map.jsx
+++ client/containers/Map.jsx
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
 import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
 
-export default function Map (props) {
+export default function Map () {
+  const markers = [
+    {
+      defaultPosition: {
+        lat: 37.7749,
+        lng: -122.4194
+      }
+    }
+  ];
+
   return (
       <GoogleMapLoader
-        containerElement={
-          <div
-            className="map"
-            {...props.containerElementProps}
-          />
-        }
+        containerElement={<div className="map"/>}
         googleMapElement={
           <GoogleMap
-            ref={(map) => console.log(map)}
+            ref={map => console.log(map)}
             defaultZoom={12}
             defaultCenter={{
               lat: 37.7749,
               lng: -122.4194
             }}
-            onClick={props.onMapClick}
           >
-            {props.markers.map((marker, index) => {
-              return (
-                <Marker
-                  ref={(marker) => console.log(marker)}
-                  key={index}
-                  {...marker}
-                  onRightclick={() => props.onMarkerRightclick(index)}
-                />
-              );
-            })}
+            {markers.map((marker, index) =>
+              <Marker
+                ref={marker => console.log(marker)}
+                key={index}
+                {...marker}
+              />
+            )}
           </GoogleMap>
         }
       />
