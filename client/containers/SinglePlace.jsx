18bc02a73358c7287f9e2077b1d7df8cc80c0fc3
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-29 20:26:21 -0800

    [MODIFY](Client): Create basic single suggestion {VE}

--- client/containers/SinglePlace.jsx
+++ client/containers/SinglePlace.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import StarRating from 'react-bootstrap-star-rating';
 
 class SinglePlace extends React.Component {
+
+  dollarSigns () {
+    const price = this.props.singleListing.price_level;
+    let dollaz = '';
+    for(var i = 0; i < price; i++) {
+      dollaz+='$';
+    }
+    return dollaz;
+  }
+
+  creatRating () {
+    //figure out how to get partial stars
+  }
+
   createListing () {
-    console.log(this.props.singleListing)
-    return this.props.singleListing.photos
+    const listing = this.props.singleListing;
+    return <div>
+        <img src="http://akns-images.eonline.com/eol_images/Entire_Site/2016813/rs_300x300-160913131824-600-hey-girl.jpg"/>
+        <h2>{listing.name}</h2>
+        <div>
+          <h4 style={{float: 'left'}}>{listing.rating}/5</h4> 
+          <h4 style={{float: 'right'}}>{this.dollarSigns()}</h4>
+        </div>
+      </div>;
   }
 
   render () {
     return (
-      <ul>
-        <li>{this.createListing()}</li>
-      </ul>
+      <div className='col-sm-4'>
+        {this.createListing()}
+      </div>
     )
   }
 }
