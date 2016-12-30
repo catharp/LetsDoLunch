b4a3f71aedd05c8472a90a27bb5f8a55dd5b6e21
zeninspire <zeninspire@gmail.com>
2016-12-30 10:33:01 -0800

    [MODIFY](Server): installed babel-plugin-transform-object-rest-spread for '...' to work for objects. {AZ}

--- client/reducers/preference_reducer.js
+++ client/reducers/preference_reducer.js
@@ -19,13 +19,19 @@ export default (state = initialPrefState, action) => {
       let allTStatus = state.timeStatus
       let currTStatus = state.timeStatus[action.timeChosen]
       allTStatus[action.timeChosen] = !currTStatus
-      const newState = Object.assign({}, state)
-      newState.timeStatus = allTStatus;
-      return newState;
-      // return {
-      //   ...state,
-      //   timeStatus: allTStatus
-      // }
+      // const newState = Object.assign({}, state)
+      // newState.timeStatus = allTStatus;
+      // return newState;
+
+      //... only works for [], not {} in ES6.
+      //in order to get around that, we can do either line 22-24, or install babel-plugin-transform-object-rest-spread, and add a plugin in webpack config.
+      //http://redux.js.org/docs/recipes/UsingObjectSpreadOperator.html
+
+      return {
+        ...state,
+        timeStatus: allTStatus
+      }
+
     default:
       return state;
   }
