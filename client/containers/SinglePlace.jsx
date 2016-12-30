572af927dfe726b84ddc7db75485bb16f3236c06
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-30 10:51:33 -0800

    [MODIFY](Client): Add simple styling to single rec {VE}

--- client/containers/SinglePlace.jsx
+++ client/containers/SinglePlace.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import StarRating from 'react-bootstrap-star-rating';
+import { Glyphicon, Image } from 'react-bootstrap'
 
 class SinglePlace extends React.Component {
 
@@ -14,24 +15,29 @@ class SinglePlace extends React.Component {
   }
 
   creatRating () {
-    //figure out how to get partial stars
+    //eventually use this to render rating as stars
   }
 
   createListing () {
     const listing = this.props.singleListing;
+    //currently with dummy image because have to make call to google for photo
     return <div>
-        <img src="http://akns-images.eonline.com/eol_images/Entire_Site/2016813/rs_300x300-160913131824-600-hey-girl.jpg"/>
+      <Image src="http://akns-images.eonline.com/eol_images/Entire_Site/2016813/rs_300x300-160913131824-600-hey-girl.jpg" responsive />
         <h2>{listing.name}</h2>
         <div>
-          <h4 style={{float: 'left'}}>{listing.rating}/5</h4> 
-          <h4 style={{float: 'right'}}>{this.dollarSigns()}</h4>
+          <h4 className="col-sm-6">Rating: {listing.rating}/5</h4> 
+          <h4 className="col-sm-6">Price Level: {this.dollarSigns()}</h4>
         </div>
-      </div>;
+        <div>
+          <Glyphicon className="col-sm-6 btn btn-danger" glyph="remove" />
+          <Glyphicon className="col-sm-6 btn btn-success" glyph="ok" />
+        </div>
+      </div>
   }
 
   render () {
     return (
-      <div className='col-sm-4'>
+      <div className='col-sm-6 single-rec'>
         {this.createListing()}
       </div>
     )
