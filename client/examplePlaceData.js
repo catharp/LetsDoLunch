7fe9e13c90f4ab19a9db5cd91f0ec46efd56aaef
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-30 16:47:23 -0800

    [MODIFY](Client): Clean up sample data and single place reducer {VE}

---
+++ client/examplePlaceData.js
@@ -0,0 +1,300 @@
+const data = {
+  "region": {
+    "span": {
+      "latitude_delta": 0.02248267999999598,
+      "longitude_delta": 0.022446205439905498
+    },
+    "center": {
+      "latitude": 37.7879456,
+      "longitude": -122.40273683821749
+    }
+  },
+  "total": 40,
+  "businesses": [
+    {
+      "is_claimed": true,
+      "rating": 4.5,
+      "mobile_url": "https://m.yelp.com/biz/cafe-du-soleil-san-francisco-4?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "rating_img_url": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
+      "review_count": 283,
+      "name": "Cafe du Soleil",
+      "snippet_image_url": "https://s3-media3.fl.yelpcdn.com/photo/Gie_Od-XSk9_aU65qoHi3w/ms.jpg",
+      "rating_img_url_small": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
+      "url": "https://www.yelp.com/biz/cafe-du-soleil-san-francisco-4?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "categories": [
+        [
+          "French",
+          "french"
+        ],
+        [
+          "Salad",
+          "salad"
+        ],
+        [
+          "Sandwiches",
+          "sandwiches"
+        ]
+      ],
+      "phone": "4156996154",
+      "snippet_text": "What a great little hidden gem! Made us feel like we were back in Paris. \n\nThe owner clearly cares so much about the food he serves here. The service was...",
+      "image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/RnsumH60wQ7XdOlrNgdLAQ/ms.jpg",
+      "location": {
+        "cross_streets": "Folsom St & Saint Francis Pl",
+        "city": "San Francisco",
+        "display_address": [
+          "345 3rd St",
+          "SoMa",
+          "San Francisco, CA 94107"
+        ],
+        "geo_accuracy": 9.5,
+        "neighborhoods": [
+          "SoMa"
+        ],
+        "postal_code": "94107",
+        "country_code": "US",
+        "address": [
+          "345 3rd St"
+        ],
+        "coordinate": {
+          "latitude": 37.7832674501439,
+          "longitude": -122.397890965607
+        },
+        "state_code": "CA"
+      },
+      "display_phone": "+1-415-699-6154",
+      "rating_img_url_large": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png",
+      "id": "cafe-du-soleil-san-francisco-4",
+      "is_closed": false,
+      "distance": 501.54611885727894
+    },
+    {
+      "is_claimed": true,
+      "rating": 4.5,
+      "mobile_url": "https://m.yelp.com/biz/sutter-st-cafe-san-francisco?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "rating_img_url": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
+      "review_count": 105,
+      "name": "Sutter St. Cafe",
+      "snippet_image_url": "https://s3-media2.fl.yelpcdn.com/photo/1NVe8eoWV8i8jA1X1kSaLQ/ms.jpg",
+      "rating_img_url_small": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
+      "url": "https://www.yelp.com/biz/sutter-st-cafe-san-francisco?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "categories": [
+        [
+          "Cafes",
+          "cafes"
+        ],
+        [
+          "Delis",
+          "delis"
+        ]
+      ],
+      "menu_date_updated": 1482547434,
+      "phone": "4153628342",
+      "snippet_text": "I work in this building and always look forward for having a delicious lunch at this place !!! Thank you so much delicious clean food ! Place is pretty...",
+      "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/5KtbEXlCGte1OM1K5eULsw/ms.jpg",
+      "location": {
+        "cross_streets": "Stockton St",
+        "city": "San Francisco",
+        "display_address": [
+          "450 Sutter St",
+          "Union Square",
+          "San Francisco, CA 94108"
+        ],
+        "geo_accuracy": 9.5,
+        "neighborhoods": [
+          "Union Square"
+        ],
+        "postal_code": "94108",
+        "country_code": "US",
+        "address": [
+          "450 Sutter St"
+        ],
+        "coordinate": {
+          "latitude": 37.7894760577176,
+          "longitude": -122.407699611023
+        },
+        "state_code": "CA"
+      },
+      "display_phone": "+1-415-362-8342",
+      "rating_img_url_large": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png",
+      "menu_provider": "eat24",
+      "id": "sutter-st-cafe-san-francisco",
+      "is_closed": false,
+      "distance": 724.6993405682266
+    },
+    {
+      "is_claimed": false,
+      "rating": 4.5,
+      "mobile_url": "https://m.yelp.com/biz/lous-cafe-san-francisco-5?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "rating_img_url": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
+      "review_count": 63,
+      "name": "Lou's Cafe",
+      "snippet_image_url": "https://s3-media1.fl.yelpcdn.com/photo/AdcW9FSrHPXXporz-NLrBw/ms.jpg",
+      "rating_img_url_small": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
+      "url": "https://www.yelp.com/biz/lous-cafe-san-francisco-5?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "categories": [
+        [
+          "Salad",
+          "salad"
+        ],
+        [
+          "Soup",
+          "soup"
+        ],
+        [
+          "Sandwiches",
+          "sandwiches"
+        ]
+      ],
+      "menu_date_updated": 1476441922,
+      "phone": "4152834777",
+      "snippet_text": "The price is in line with other places in FIDI, they serve breakfast and lunch, and they have a seating area/ court yard to hang out and eat your food.\n\nThe...",
+      "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/--_8ZoP_rTQ1CBjvvKviSg/ms.jpg",
+      "location": {
+        "cross_streets": "Front St & Battery St",
+        "city": "San Francisco",
+        "display_address": [
+          "100 Pine St",
+          "Ste 102",
+          "Financial District",
+          "San Francisco, CA 94111"
+        ],
+        "geo_accuracy": 8,
+        "neighborhoods": [
+          "Financial District"
+        ],
+        "postal_code": "94111",
+        "country_code": "US",
+        "address": [
+          "100 Pine St",
+          "Ste 102"
+        ],
+        "coordinate": {
+          "latitude": 37.79247,
+          "longitude": -122.39891
+        },
+        "state_code": "CA"
+      },
+      "display_phone": "+1-415-283-4777",
+      "rating_img_url_large": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png",
+      "menu_provider": "eat24",
+      "id": "lous-cafe-san-francisco-5",
+      "is_closed": false,
+      "distance": 573.7697960114007
+    },
+    {
+      "is_claimed": true,
+      "rating": 4.5,
+      "mobile_url": "https://m.yelp.com/biz/tacorea-san-francisco?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "rating_img_url": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
+      "review_count": 367,
+      "name": "Tacorea",
+      "snippet_image_url": "https://s3-media4.fl.yelpcdn.com/photo/omxjzm9XP239vrLlNDRgqw/ms.jpg",
+      "rating_img_url_small": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
+      "url": "https://www.yelp.com/biz/tacorea-san-francisco?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "categories": [
+        [
+          "Mexican",
+          "mexican"
+        ],
+        [
+          "Korean",
+          "korean"
+        ],
+        [
+          "Latin American",
+          "latin"
+        ]
+      ],
+      "menu_date_updated": 1472899698,
+      "phone": "4158851325",
+      "snippet_text": "This place does Korean - Mexican fusion so well! The prices are also super fair for San Francisco. \n\nMy friends and I were about to get two The Big...",
+      "image_url": "https://s3-media1.fl.yelpcdn.com/bphoto/7sOn8_P7Hxb0ZkM55tz3mg/ms.jpg",
+      "location": {
+        "cross_streets": "Taylor St & Mason St",
+        "city": "San Francisco",
+        "display_address": [
+          "809 Bush St",
+          "Union Square",
+          "San Francisco, CA 94108"
+        ],
+        "geo_accuracy": 9.5,
+        "neighborhoods": [
+          "Union Square",
+          "Lower Nob Hill"
+        ],
+        "postal_code": "94108",
+        "country_code": "US",
+        "address": [
+          "809 Bush St"
+        ],
+        "coordinate": {
+          "latitude": 37.789806,
+          "longitude": -122.410709
+        },
+        "state_code": "CA"
+      },
+      "display_phone": "+1-415-885-1325",
+      "rating_img_url_large": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png",
+      "menu_provider": "single_platform",
+      "id": "tacorea-san-francisco",
+      "is_closed": false,
+      "distance": 988.8962948907355
+    },
+    {
+      "is_claimed": true,
+      "rating": 4.5,
+      "mobile_url": "https://m.yelp.com/biz/rove-kitchen-san-francisco?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "rating_img_url": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png",
+      "review_count": 319,
+      "name": "Rove Kitchen",
+      "snippet_image_url": "https://s3-media1.fl.yelpcdn.com/photo/5fHvJJAXz7kHCfr4e3kiRQ/ms.jpg",
+      "rating_img_url_small": "https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png",
+      "url": "https://www.yelp.com/biz/rove-kitchen-san-francisco?adjust_creative=1RZzJ1VRGOtiSobrmUQA3A&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=1RZzJ1VRGOtiSobrmUQA3A",
+      "categories": [
+        [
+          "Burgers",
+          "burgers"
+        ],
+        [
+          "American (Traditional)",
+          "tradamerican"
+        ]
+      ],
+      "menu_date_updated": 1472893805,
+      "phone": "4155677466",
+      "snippet_text": "We were in the area for a show at the Masonic, and wanted to eat here, but they don't take reservations. We didn't want to be late for the show, so we made...",
+      "image_url": "https://s3-media4.fl.yelpcdn.com/bphoto/COB2_0YdYHyX1264bexFLQ/ms.jpg",
+      "location": {
+        "cross_streets": "Jones St & Ophir Aly",
+        "city": "San Francisco",
+        "display_address": [
+          "678 Post St",
+          "Lower Nob Hill",
+          "San Francisco, CA 94109"
+        ],
+        "geo_accuracy": 9.5,
+        "neighborhoods": [
+          "Lower Nob Hill"
+        ],
+        "postal_code": "94109",
+        "country_code": "US",
+        "address": [
+          "678 Post St"
+        ],
+        "coordinate": {
+          "latitude": 37.7877664572471,
+          "longitude": -122.412939658872
+        },
+        "state_code": "CA"
+      },
+      "display_phone": "+1-415-567-7466",
+      "rating_img_url_large": "https://s3-media4.fl.yelpcdn.com/assets/2/www/img/9f83790ff7f6/ico/stars/v1/stars_large_4_half.png",
+      "menu_provider": "single_platform",
+      "id": "rove-kitchen-san-francisco",
+      "is_closed": false,
+      "distance": 1151.6895733040794
+    }
+  ]
+}
+
+export default data;
