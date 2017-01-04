8072df9add86a26d7e5679d441817ddf5beee22f
Collin Tharp <catharp@gmail.com>
2017-01-04 12:24:57 -0800

    [MODIFY](client): refactor map component to functional stateless {CT}

--- client/components/Map_Component.jsx
+++ client/components/Map_Component.jsx
@@ -3,54 +3,48 @@ import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 
 var directionsService = new google.maps.DirectionsService();
 var directionsDisplay = new google.maps.DirectionsRenderer();
+var map;
 
-export default class Map_Component extends Component {
-
-  constructor(props) {
-    super(props);
+export default ({zoom, center, changeBounds, origin, changeOrigin, destination}) => {
+  if (destination) {
+    let request = {
+      origin: new google.maps.LatLng(origin.lat, origin.lng),
+      destination: new google.maps.LatLng(destination.lat, destination.lng),
+      travelMode: 'WALKING'
+    };
+  directionsService.route(request, (response, status) => {
+    if (status == 'OK') directionsDisplay.setDirections(response);
+  });
   }
-
-  render() {
-    if (this.props.destination) {
-      let request = {
-        origin: new google.maps.LatLng(this.props.origin.lat, this.props.origin.lng),
-        destination: new google.maps.LatLng(this.props.destination.lat, this.props.destination.lng),
-        travelMode: 'WALKING'
-      };
-    directionsService.route(request, (response, status) => {
-      if (status == 'OK') directionsDisplay.setDirections(response);
-    });
-    }
-    return (
-      <div>
-        <GoogleMapLoader
-          containerElement={<div className='map' />}
-          googleMapElement={
-            <GoogleMap
-              ref={googleMap => {
-                if (googleMap) directionsDisplay.setMap(this.map = googleMap.props.map)
-              }}
-              zoom={this.props.zoom}
-              center={this.props.center}
-              onBoundsChanged={() => {
-                this.props.changeBounds({
-                  zoom: this.map.zoom,
-                  center: {lat: this.map.center.lat(), lng: this.map.center.lng()}
-                })
-              }}
-              onClick={click => this.props.destination ? null : this.props.changeOrigin({lat: click.latLng.lat(), lng: click.latLng.lng()})}
-            >
-              {this.props.destination ?
-                null :
-                <Marker
-                key={this.props.origin.lat + '' + this.props.origin.lng}
-                defaultPosition={this.props.origin}
-                />
-              }
-            </GoogleMap>
-          }
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <GoogleMapLoader
+        containerElement={<div className='map' />}
+        googleMapElement={
+          <GoogleMap
+            ref={googleMap => {
+              if (googleMap) directionsDisplay.setMap(map = googleMap.props.map)
+            }}
+            zoom={zoom}
+            center={center}
+            onBoundsChanged={() => {
+              changeBounds({
+                zoom: map.zoom,
+                center: {lat: map.center.lat(), lng: map.center.lng()}
+              })
+            }}
+            onClick={click => destination ? null : changeOrigin({lat: click.latLng.lat(), lng: click.latLng.lng()})}
+          >
+            {destination ?
+              null :
+              <Marker
+              key={origin.lat + '' + origin.lng}
+              defaultPosition={origin}
+              />
+            }
+          </GoogleMap>
+        }
+      />
+    </div>
+  );
 }
