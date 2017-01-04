cc45f356b3e006760d6ea4d4aa2a1b67bb281e75
Collin Tharp <catharp@gmail.com>
2017-01-04 11:37:24 -0800

    [MODIFY](client): freeze origin when destination exists {CT}

--- client/components/Map_Component.jsx
+++ client/components/Map_Component.jsx
@@ -23,16 +23,18 @@ export default class Map_Component extends Component {
                   center: {lat: this.map.props.map.center.lat(), lng: this.map.props.map.center.lng()}
                 })
               }}
-              onClick={click => this.props.changeOrigin({lat: click.latLng.lat(), lng: click.latLng.lng()})}
+              onClick={click => this.props.destination ? null : this.props.changeOrigin({lat: click.latLng.lat(), lng: click.latLng.lng()})}
             >
               <Marker
                 key={this.props.origin.lat + '' + this.props.origin.lng}
                 defaultPosition={this.props.origin}
               />
-              <Marker
-                key={this.props.destination.lat + '' + this.props.destination.lng}
-                defaultPosition={this.props.destination}
-              />
+              {this.props.destination ?
+                <Marker
+                  key={this.props.destination.lat + '' + this.props.destination.lng}
+                  defaultPosition={this.props.destination}
+                /> : null
+              }
             </GoogleMap>
           }
         />
