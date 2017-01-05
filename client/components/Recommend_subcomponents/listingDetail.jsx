e61adafd3a01aafd79040f15535939a55cd18bd8
Collin Tharp <catharp@gmail.com>
2017-01-05 15:47:40 -0800

    [MODIFY](client): reformat listing info {CT}

--- client/components/Recommend_subcomponents/listingDetail.jsx
+++ client/components/Recommend_subcomponents/listingDetail.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const listingDetail = (listing) => {
   return (
     <div>
-      <h4 className="col-sm-6">Distance: {listing.distance}</h4>
-      <h4 className="col-sm-6">Travel Time: {listing.duration}</h4>
-      <br />
-      <h4 className="col-sm-6">Phone: {listing.display_phone}</h4>
-      <h4 className="col-sm-6">Address: {listing.location.display_address}</h4>
+      <h4>Distance: {listing.distance}, {listing.duration} away</h4>
+      <h4>Phone: {listing.display_phone}</h4>
+      <h4>Address: {listing.location.display_address}</h4>
     </div>
   )
 }
