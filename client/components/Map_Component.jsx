119005a7da0732db5fd4a880c83a0eb8cd5e5860
Collin Tharp <catharp@gmail.com>
2017-01-05 14:47:16 -0800

    [MODIFY](client): refactor map components to be classier {CT}

--- client/components/Map_Component.jsx
+++ client/components/Map_Component.jsx
@@ -5,48 +5,60 @@ var directionsService = new google.maps.DirectionsService();
 var directionsDisplay = new google.maps.DirectionsRenderer();
 var map;
 
-export default ({zoom, center, changeBounds, origin, changeOrigin, destination}) => {
-  if (destination) {
-    let request = {
-      origin: new google.maps.LatLng(origin.lat, origin.lng),
-      destination: new google.maps.LatLng(destination.lat, destination.lng),
-      travelMode: 'WALKING'
-    };
-  directionsService.route(request, (response, status) => {
-    if (status == 'OK') directionsDisplay.setDirections(response);
-  });
+export default class Map_Component extends Component {
+
+  constructor(props) {
+    super(props)
   }
-  return (
-    <div>
-      <GoogleMapLoader
-        containerElement={<div className='map' />}
-        googleMapElement={
-          <GoogleMap
-            ref={googleMap => {
-              if (googleMap) {
-                directionsDisplay.setMap(map = googleMap.props.map)
+
+  componentDidUpdate() {
+    let { origin, destination } = this.props;
+    if (destination) {
+      let request = {
+        origin: new google.maps.LatLng(origin.lat, origin.lng),
+        destination: new google.maps.LatLng(destination.lat, destination.lng),
+        travelMode: 'WALKING'
+      };
+      directionsService.route(request, (response, status) => {
+        if (status == 'OK') directionsDisplay.setDirections(response);
+      });
+    }
+  }
+
+  render() {
+    let { zoom, center, changeBounds, origin, changeOrigin, destination } = this.props;
+    return (
+      <div>
+        <GoogleMapLoader
+          containerElement={<div className='map' />}
+          googleMapElement={
+            <GoogleMap
+              ref={googleMap => {
+                if (googleMap) {
+                  directionsDisplay.setMap(map = googleMap.props.map)
+                }
+              }}
+              zoom={zoom}
+              center={center}
+              onBoundsChanged={() => {
+                changeBounds({
+                  zoom: map.zoom,
+                  center: {lat: map.center.lat(), lng: map.center.lng()}
+                })
+              }}
+              onClick={click => destination ? null : changeOrigin({lat: click.latLng.lat(), lng: click.latLng.lng()})}
+            >
+              {destination ?
+                null :
+                <Marker
+                key={origin.lat + '' + origin.lng}
+                defaultPosition={origin}
+                />
               }
-            }}
-            zoom={zoom}
-            center={center}
-            onBoundsChanged={() => {
-              changeBounds({
-                zoom: map.zoom,
-                center: {lat: map.center.lat(), lng: map.center.lng()}
-              })
-            }}
-            onClick={click => destination ? null : changeOrigin({lat: click.latLng.lat(), lng: click.latLng.lng()})}
-          >
-            {destination ?
-              null :
-              <Marker
-              key={origin.lat + '' + origin.lng}
-              defaultPosition={origin}
-              />
-            }
-          </GoogleMap>
-        }
-      />
-    </div>
-  );
+            </GoogleMap>
+          }
+        />
+      </div>
+    );
+  }
 }
