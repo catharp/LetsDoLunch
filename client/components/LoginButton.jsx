25e6b0d853ec1669ef8669746217367fd4ac845d
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-05 22:44:44 -0800

    [FIX](Client): fix merge conflicts {VE}

--- client/components/LoginButton.jsx
+++ client/components/LoginButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const loginButton = () => {
-  return <Button className="btn btn-primary">Login</Button>
+const loginButton = ({onClick}) => {
+  return <Button className="btn btn-primary" onClick={onClick}>Login</Button>
 }
 
 export default loginButton;
--- client/index.js
+++ client/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './Routes.jsx';
 import reducers from './reducers/reducer_index';
+import thunkMiddleware from 'redux-thunk';
 
 const appStore = createStore(reducers,
   applyMiddleware(thunkMiddleware)
