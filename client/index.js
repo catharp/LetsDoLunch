4e8e405ad6381822ac670d0265d50754215bab58
Nate Hackman <nathan.hackman@gmail.com>
2017-01-06 15:50:20 -0800

    [MODIFY](Client): Modify Store Creation To Allow Redux Devtool Integration {NH}

--- client/index.js
+++ client/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './Routes.jsx';
 import reducers from './reducers/reducer_index';
 import thunkMiddleware from 'redux-thunk';
 
-const appStore = createStore(reducers,
-  applyMiddleware(thunkMiddleware)
-);
+// const appStore = createStore(reducers,
+//   applyMiddleware(thunkMiddleware)
+// );
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ));
 
 ReactDOM.render(
-  <Provider store={appStore}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
