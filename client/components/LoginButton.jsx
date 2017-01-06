633d49daa5e29dc35b81327b39e8c7694bdc1898
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-05 19:29:54 -0800

    [MODIFY](Client): Refeactor navbar to be stateless {VE}

---
+++ client/components/LoginButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const loginButton = () => {
+  return <Button className="btn btn-primary">Login</Button>
+}
+
+export default loginButton;
--- client/containers/Navbar.jsx
+++ client/containers/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 //start of testing feel lucky component
 import fetch from 'isomorphic-fetch'
+import LoginButton from '../components/LoginButton.jsx'
 import { connect } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { Button } from 'react-bootstrap';
@@ -9,23 +10,23 @@ import Lucky from '../components/Preference_subcomponent/Lucky.jsx';
 
 import { fetchPlaces, receivePlaces, filterPlaces } from '../actions/action_get_places';
 
-class Navbar extends Component {
-  render() {
-    return (
+const Navbar = ({feelingLucky}) => {
+  return (
       <nav className="navbar navbar-default">
 
-      <div className="col-md-1"><Lucky feelingLucky={this.props.feelingLucky}/></div>
+      <div className="col-md-1"><Lucky feelingLucky={feelingLucky}/></div>
 
 
         <ul>
+          <li><LoginButton /></li>
           <li><a href="/dog">Dog</a></li>
           <li><a href="/recommend">Recommend</a></li>
           <li><a href="/search">Search</a></li>
         </ul>
       </nav>
     );
-  }
 }
+
 
 const mapDispatchToProps = (dispatch) => ({
   feelingLucky: () => {
@@ -39,9 +40,8 @@ const mapDispatchToProps = (dispatch) => ({
   }
 })
 
-Navbar = connect(
+export default connect(
   null,
   mapDispatchToProps
 )(Navbar)
 
-export default Navbar
