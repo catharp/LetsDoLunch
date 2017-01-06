5a4369d9ead3d3aa5b24e0ec3e39ec7729a71336
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-05 20:23:23 -0800

    [MODIFY](Client): Add thunk middleware {VE}

--- client/index.js
+++ client/index.js
@@ -8,18 +8,13 @@ import routes from './Routes.jsx';
 import reducers from './reducers/reducer_index';
 import thunkMiddleware from 'redux-thunk';
 
-// const appStore = createStore(reducers,
-//   applyMiddleware(thunkMiddleware)
-// );
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(reducers, composeEnhancers(
-    applyMiddleware(thunkMiddleware)
-  ));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(thunkMiddleware)
+));
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.app'));
-
