c21ae36f71677cea6e6ef9bdc07ee83deea536c2
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-06 19:43:40 -0800

    [MODIFY](Client): Refactor navbar into component {VE}

--- client/containers/Navbar.jsx
+++ client/containers/Navbar.jsx
@@ -1,31 +1,10 @@
-import React, { Component } from 'react';
-//start of testing feel lucky component
 import fetch from 'isomorphic-fetch'
-import LoginButton from '../components/LoginButton.jsx'
 import { connect } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { Button, Navbar, NavItem, Nav, MenuItem, NavDropdown } from 'react-bootstrap';
-
-import Lucky from '../components/Preference_subcomponent/Lucky.jsx';
-
 import { fetchPlaces, receivePlaces, filterPlaces } from '../actions/action_get_places';
 import FacebookLogin from '../actions/action_login'
+import Navigationbar from '../components/NavBar.jsx'
 
-const Navigationbar = ({feelingLucky, facebookLoginButton}) => {
-  return (
-    <Navbar>
-      <Nav>
-        <NavItem><Lucky feelingLucky={feelingLucky}/></NavItem>
-        <NavItem onClick={()=>browserHistory.push("/search")}>Search</NavItem>
-        <NavItem onClick={()=>browserHistory.push("/recommend")}>Recommend</NavItem>
-        <NavItem onClick={()=>browserHistory.push("/profile")}>Profile</NavItem>
-        <NavItem onClick={()=>browserHistory.push("/dog")}>Dog</NavItem>
-        <NavItem><LoginButton onClick={() => browserHistory.push("/login")} /></NavItem>
-      </Nav>
-    </Navbar>
-      
-  );
-}
+
 
 
 const mapDispatchToProps = (dispatch) => ({
--- client/index.js
+++ client/index.js
@@ -21,5 +21,5 @@ ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.app'));
 
