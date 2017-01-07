c8a68101c4cd98d90ac8dd8b99e8f3a24ebaa0f4
Nate Hackman <nathan.hackman@gmail.com>
2017-01-07 15:03:05 -0800

    [MODIFY](Database): Add Robustness to Database Queries {NH}

--- server/utils/db_handler.js
+++ server/utils/db_handler.js
@@ -13,6 +13,10 @@ var db = mysql.createConnection({
 
 var { query, checkingQuery } = require('./db/promisified_mysql')(db);
 
+let userQuery = function(user) {
+  return user.sessionUsername ? `fbname="${user.sessionUsername}"` : `username="${user.username}"`;
+}
+
 db.connect(function(err) {
   if (err) console.error('Error establishing database connection:', err);
   else console.log('Database connection established!');
@@ -28,7 +32,7 @@ module.exports.addUser = function(user, token) {
     .then(() => user);
   }
 
-  return checkingQuery(`SELECT * FROM users WHERE username="${user.username}"`)
+  return checkingQuery(`SELECT * FROM users WHERE ${userQuery(user)}`)
   .then(() => query('INSERT INTO users SET ?', user))
   .then(() => user);
 }
@@ -42,15 +46,15 @@ module.exports.addListing = function(listing) {
 module.exports.addUserPreference = function(user, preference) {
   let qs1 = 
   `SELECT * FROM users INNER JOIN\
-  preferences_users as p on p.user_id=users.id INNER JOIN\
-  preferences as ps on ps.id=p.preference_id where\
-  username="${user.username}" and\
+  preferences_users as p ON p.user_id=users.id INNER JOIN\
+  preferences as ps ON ps.id=p.preference_id where\
+  ${userQuery(user)} and\
   ps.name="${preference.name}"`;
     
   let qs2 = 
   `INSERT INTO preferences_users (preference_id, user_id, type) VALUES\
   ((SELECT id FROM preferences WHERE name="${preference.name}"),\
-  (SELECT id FROM users WHERE username="${user.username}"),\
+  (SELECT id FROM users WHERE ${userQuery(user)}),\
   "${preference.type}")`;
 
   return checkingQuery(qs1)
@@ -60,13 +64,13 @@ module.exports.addUserPreference = function(user, preference) {
 module.exports.addUserListing = function(user, listing) {
   let qs1 = 
   `SELECT * FROM users INNER JOIN listings_users as l\
-  on l.user_id=users.id INNER JOIN listings as ls\
-  on ls.id=l.listing_id where username="${user.username}"\
+  ON l.user_id=users.id INNER JOIN listings as ls\
+  ON ls.id=l.listing_id WHERE ${userQuery(user)}\
    and ls.name="${listing.name}"`;
   let qs2 = 
   `INSERT INTO listings_users (listing_id, user_id, type) VALUES\
   ((SELECT id FROM listings WHERE name="${listing.name}"),\
-  (SELECT id FROM users WHERE username="${user.username}"),\
+  (SELECT id FROM users WHERE ${userQuery(user)}),\
   "${listing.type}")`;
   
   return checkingQuery(qs1)
@@ -76,25 +80,25 @@ module.exports.addUserListing = function(user, listing) {
 module.exports.getUserPreferences = function(user) {
   let qs = 
   `SELECT ps.name, p.type FROM preferences_users as p\
-  INNER JOIN preferences as ps on ps.id=p.preference_id\
+  INNER JOIN preferences as ps ON ps.id=p.preference_id\
   WHERE p.user_id=(SELECT id FROM users\
-  WHERE username="${user.username}")`;
+  WHERE ${userQuery(user)})`;
   
   return query(qs);
 }
 
 // // query for listings preferences by user
 //   `SELECT ls.name, l.type FROM listings_users as l\
-//   INNER JOIN listings as ls on ls.id=l.listing_id\
+//   INNER JOIN listings as ls ON ls.id=l.listing_id\
 //   WHERE ls.id=(SELECT id FROM users\
-//   WHERE username="${user.username}")`;
+//   WHERE ${userQuery(user)})`;
 
 module.exports.getUserListings = function(user) {
   let qs = 
   `SELECT ls.name, l.type FROM listings_users as l\
-  INNER JOIN listings as ls on ls.id=l.listing_id\
+  INNER JOIN listings as ls ON ls.id=l.listing_id\
   WHERE l.user_id=(SELECT id FROM users\
-  WHERE username="${user.username}")`;
+  WHERE ${userQuery(user)})`;
   
   return query(qs);
 }
