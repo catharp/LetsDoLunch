6277b4a99346b9195bb6c55d5619c0f3dd4d15c0
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-10 12:12:22 -0800

    [MODIFY](Client): Add button on jumbotron to redirect to search page {VE}

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 import { Jumbotron, Button } from 'react-bootstrap';
 import Selector from '../containers/Selector.jsx';
 import InitialQuestion from './InitialQuestion.jsx';
@@ -12,6 +13,7 @@ export default () => {
       <div className='container'>
         <h1>Wanna Hang Out?</h1>
         <InitialQuestion />
+        <Button onClick={() => browserHistory.push('/search')}>Let's Go!</Button>
       </div>
     </Jumbotron>
 
