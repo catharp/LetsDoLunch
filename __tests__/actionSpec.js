2512d1d4bef63663585add3e2d4d75ad168400c1
Collin Tharp <catharp@gmail.com>
2017-01-10 16:34:39 -0800

    [INIT](test): write tests for map actions {CT}

--- __tests__/actionSpec.js
+++ __tests__/actionSpec.js
@@ -1,10 +1,16 @@
 import { changeBounds, changeOrigin } from '../client/actions/map_action.js'
 
-describe('actions', () => {
+describe('Map Actions', () => {
   describe('changeBounds', () => {
     it('should pass along the bounds we pass in', () => {
       let bounds = {zoom: 16, center: 'somewhere'}
       expect(changeBounds(bounds).newBounds).toEqual(bounds)
+    })
+  })
+  describe('changeOrigin', () => {
+    it('should pass along the origin we pass in', () => {
+      let origin = {lat: 'northish', lng: 'westy'}
+      expect(changeOrigin(origin).newOrigin).toEqual(origin)
     })
   })
 })
