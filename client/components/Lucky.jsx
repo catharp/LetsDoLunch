e40aadd44c881a4bb95132745657442c4a740e81
Collin Tharp <catharp@gmail.com>
2017-01-10 19:12:18 -0800

    [MODIFY](client): begin refactoring lucky button {CT}

--- client/components/Lucky.jsx
+++ client/components/Lucky.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import fetch from 'isomorphic-fetch';
 
 
 export default ({ query, setQuery, startFetch }) => (
   <div className='lucky'>
     <Button bsStyle='warning' onClick={() => {
+
+      fetch('/db/userpreferences?type=Steff%27s&name=Steff%27s', {
+        method: 'POST',
+        credentials: 'same-origin'
+      })
+      .then(res => console.log('posting...', res.json()))
+
+      fetch('db/userpreferences', {
+        credentials: 'same-origin'
+      })
+      .then(res => console.log('getting...', res.json()))
+
       setQuery({...query, cuisine: {Gold: true, Club: true}});
       startFetch();
     }
