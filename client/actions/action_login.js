bb61caf48abbc50ac63754e2c7d2f0579481aea8
Collin Tharp <catharp@gmail.com>
2017-01-10 16:51:10 -0800

    [MODIFY](client): merge authentication and login action files {CT}

--- client/actions/action_login.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'isomorphic-fetch';
-
-const facebookLogin = () => (
-  fetch('/auth/facebook')
-  .then((resp) => { console.log(resp); })
-  .catch((err) => { console.log(err); })
-);
-
-export default facebookLogin;
