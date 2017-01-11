cdc050497e1eacc94284ddcd79a9371ddfc46022
Collin Tharp <catharp@gmail.com>
2017-01-10 17:14:14 -0800

    [INIT](test): write tests for get places actions {CT}

--- __tests__/actionSpec.js
+++ __tests__/actionSpec.js
@@ -1,4 +1,5 @@
 import { changeBounds, changeOrigin } from '../client/actions/map_action.js'
+import { setQuery, updatePlaces } from '../client/actions/action_get_places.js'
 
 describe('Map Actions', () => {
   describe('changeBounds', () => {
@@ -11,6 +12,21 @@ describe('Map Actions', () => {
     it('should pass along the origin we pass in', () => {
       let origin = {lat: 'northish', lng: 'westy'}
       expect(changeOrigin(origin).newOrigin).toEqual(origin)
+    })
+  })
+})
+
+describe('Get Places Actions', () => {
+  describe('setQuery', () => {
+    it('should pass along the query we pass in', () => {
+      let query = {stuff: 'things'}
+      expect(setQuery(query).query).toEqual(query)
+    })
+  })
+  describe('updatePlaces', () => {
+    it('should pass along the places we pass in', () => {
+      let places = [{name: 'makersquare'}, {name: 'hack reactor'}]
+      expect(updatePlaces(places).places).toEqual(places)
     })
   })
 })
