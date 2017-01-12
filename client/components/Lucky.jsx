c53870456c7b1f8eff3c98b4ddc845daf7966e1f
Collin Tharp <catharp@gmail.com>
2017-01-11 19:05:13 -0800

    [MODIFY](client): reimplement lucky button {CT}

--- client/components/Lucky.jsx
+++ client/components/Lucky.jsx
@@ -6,22 +6,21 @@ import fetch from 'isomorphic-fetch';
 export default ({ query, setQuery, startFetch }) => (
   <div className='lucky'>
     <Button bsStyle='warning' onClick={() => {
-
-      fetch('/db/userpreferences?type=Steff%27s&name=Steff%27s', {
-        method: 'POST',
-        credentials: 'same-origin'
-      })
-      .then(res => console.log('posting...', res.json()))
-
       fetch('db/userpreferences', {
         credentials: 'same-origin'
       })
-      .then(res => console.log('getting...', res.json()))
-
-      setQuery({...query, cuisine: {Gold: true, Club: true}});
-      startFetch();
-    }
-    }>Feeling Lucky!
+      .then(res => res.json())
+      .then(results => {
+        let preferences = results.preferences.reduce((preferences, pref) => {
+          preferences[pref.name] = true;
+          return preferences;
+        }, {})
+        console.log(preferences);
+        setQuery({...query, detail: preferences});
+        startFetch();
+      })
+      .catch(err => console.error(err))
+    }}>Feeling Lucky!
     </Button>
   </div>
 )
