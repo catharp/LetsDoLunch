a0380e8cc4f84f28c519fef70833ef60bfda0de9
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-12 12:00:43 -0800

    [MODIFY](Client): Add hidden class for map {VE}

--- client/components/Preferences.jsx
+++ client/components/Preferences.jsx
@@ -15,51 +15,69 @@ const prices = ['$','$$','$$$','$$$$']
 const times = ['Now', 'Anytime']
 
 
-export default ({ startFetch, query, setMap, mapSet }) => (
-  <div className='col-md-11 pref'>
-    <InitialQuestion />
 
-    <div>
-      {query.selected.options ? 
-        <div className='prefItem'>
-          <div className={mapSet ? 'col-md-6': 'col-md-4'}>
-            <h4>Where do you want to go?</h4>
-            <Button bsStyle='info' onClick={() => setMap('smallMap')}>Click me when you've set your location!</Button>
-          </div>
-          <Map className='col-md-6' />
-        </div> : null}
-    </div>
 
-    <div>
-      {mapSet ? 
-        <div className='prefItem'>
-          <h4 className='col-md-4'>When do you want to go?</h4>
-          <Selector selector='time' selections={times} />
-        </div> : null}
-    </div>
+export default class extends Component {
 
-    <div>
-      {query.selected.time ? 
-      <div>
-        <div className='prefItem'>
-          <h4 className='col-md-4'>Let's get specific.</h4>
-          <div className='col-md-8'>
-            <SpecificOptions option={query.selected.options}/>
-          </div>
+  constructor(props) {
+    super(props);
+    this.props.setMap.bind(this);
+  }
+
+  componentWillMount () {
+    this.props.setMap('hiddenMap');
+  }
+
+  render () {
+    let { startFetch, query, setMap, mapSet, mapClass } = this.props;
+
+    return (
+      <div className='col-md-11 pref'>
+        <InitialQuestion />
+
+        <div>
+          {query.selected.options ? 
+            <div className='prefItem'>
+              <div className={mapSet ? 'col-md-6': 'col-md-4'}>
+                <h4>Where do you want to go?</h4>
+                <Button bsStyle='info' onClick={() => setMap('smallMap')}>Click me when you've set your location!</Button>
+              </div>
+              <Map className='col-md-6' />
+            </div> : null}
         </div>
-        <div className='prefItem'>
-          <h4 className='col-md-4'>How much do you want to spend?</h4>
-          <div className='col-md-6'>
-            <Selector selector='price' selections={prices} /> 
-          </div>
+
+        <div>
+          {mapSet ? 
+            <div className='prefItem'>
+              <h4 className='col-md-4'>When do you want to go?</h4>
+              <Selector selector='time' selections={times} />
+            </div> : null}
         </div>
-        <div className='col-md-11'>
-          <Button bsStyle='info' onClick={() => {startFetch(); setMap('bigMap'); browserHistory.push('/recommend')}}>
-            Show Me Where To Go!
-          </Button>
+
+        <div>
+          {query.selected.time ? 
+          <div>
+            <div className='prefItem'>
+              <h4 className='col-md-4'>Let's get specific.</h4>
+              <div className='col-md-8'>
+                <SpecificOptions option={query.selected.options}/>
+              </div>
+            </div>
+            <div className='prefItem'>
+              <h4 className='col-md-4'>How much do you want to spend?</h4>
+              <div className='col-md-6'>
+                <Selector selector='price' selections={prices} /> 
+              </div>
+            </div>
+            <div className='col-md-11'>
+              <Button bsStyle='info' onClick={() => {startFetch(); setMap('bigMap'); browserHistory.push('/recommend')}}>
+                Show Me Where To Go!
+              </Button>
+            </div>
+          </div> 
+          : null}
         </div>
-      </div> 
-      : null}
-    </div>
-  </div>
-)
+      </div>
+    )
+  }
+}
