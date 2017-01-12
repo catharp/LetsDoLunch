10249342a76de00673d1a88783177addfbd289da
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-11 16:13:37 -0800

    [MODIFY](Client): Clean up search flow {VE}

--- client/components/Preferences.jsx
+++ client/components/Preferences.jsx
@@ -18,30 +18,48 @@ const times = ['Now', 'Anytime']
 export default ({ startFetch, query, setMap, mapSet }) => (
   <div className='col-md-11 pref'>
     <InitialQuestion />
-      <div>
-        {query.selected.options ? 
-          <Row className='prefItem prefMap'>
-            <div className={mapSet ? 'col-md-6': 'col-md-4'}>
+
+    <div>
+      {query.selected.options ? 
+        <div className='prefItem'>
+          <div className={mapSet ? 'col-md-6': 'col-md-4'}>
             <h4>Where do you want to go?</h4>
             <Button bsStyle='info' onClick={() => setMap('smallMap')}>Click me when you've set your location!</Button>
-            </div>
-            <Map className='col-md-6' />
-          </Row> : null}
-      </div>
-      <div>
-        {mapSet ? 
-          <div className='prefItem'>
-            <h4 className='col-md-4'>When do you want to go?</h4>
-            <Selector selector='time' selections={times} /></div> : null}
-      </div>
-      <div>
-        {query.selected.time ? 
-        <div>
-          <h4 className='col-md-4'>Let's get specific.</h4>
-          <SpecificOptions option={query.selected.options}/>
-          <Selector selector='price' selections={prices} /> 
-          <Button bsStyle='info' onClick={() => {startFetch(); setMap('bigMap'); browserHistory.push('/recommend')}}>Submit</Button>
+          </div>
+          <Map className='col-md-6' />
         </div> : null}
-      </div>
+    </div>
+
+    <div>
+      {mapSet ? 
+        <div className='prefItem'>
+          <h4 className='col-md-4'>When do you want to go?</h4>
+          <Selector selector='time' selections={times} />
+        </div> : null}
+    </div>
+
+    <div>
+      {query.selected.time ? 
+      <div>
+        <div className='prefItem'>
+          <h4 className='col-md-4'>Let's get specific.</h4>
+          <div className='col-md-8'>
+            <SpecificOptions option={query.selected.options}/>
+          </div>
+        </div>
+        <div className='prefItem'>
+          <h4 className='col-md-4'>How much do you want to spend?</h4>
+          <div className='col-md-6'>
+            <Selector selector='price' selections={prices} /> 
+          </div>
+        </div>
+        <div className='col-md-11'>
+          <Button bsStyle='info' onClick={() => {startFetch(); setMap('bigMap'); browserHistory.push('/recommend')}}>
+            Show Me Where To Go!
+          </Button>
+        </div>
+      </div> 
+      : null}
+    </div>
   </div>
 )
