32c38c43b71bdde60ca10f4aebcf8f05421a5ab1
Nate Hackman <nathan.hackman@gmail.com>
2017-01-13 12:07:22 -0800

    [MODIFY](Client): Fix Yelp Category Breaking Code {NH}

--- client/components/Recommend.jsx
+++ client/components/Recommend.jsx
@@ -58,7 +58,7 @@ export default class Recommend extends Component {
           distance: distance,
           duration: duration,
           yelpRating: rating,
-          yelpCategory: category,
+          yelpCategory: category || '',
           dollar: dollar,
           open: open,
           phone: phoneNum,
