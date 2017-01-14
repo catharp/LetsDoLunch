8f92ae5f74676a88729039c2cc0232e3dd66b0c2
Valerie Ernst <valerie.m.ernst@gmail.com>
2017-01-14 11:42:19 -0800

    [MODIFY](Client): Style login page {VE}

--- client/components/LoginForm.jsx
+++ client/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form';
-import { Button } from 'react-bootstrap';
+import { Field, reduxForm } from 'redux-form'
+import { Button } from 'react-bootstrap'
 
 const PreferenceInput = (props) => {
   const { handleSubmit, pristine, reset } = props
@@ -8,10 +8,13 @@ const PreferenceInput = (props) => {
     <form className='form-group' onSubmit={handleSubmit}>
       <div>
         <label>
-          <h4>Help us help you! Tell us what you like:</h4>
+          <h5>...or enter a username and password.</h5>
         </label>
         <div className='form-input'>
-          <Field className='form-control' name="name" component="input" type="text" placeholder="Example: Chinese, Dive Bars, etc."/>
+          <Field className='form-control' name="username" component="input" type="text" placeholder="Username or Email Address"/>
+        </div>
+        <div className='form-input'>
+          <Field className='form-control' name="password" component="input" type="text" placeholder="Password"/>
         </div>
       </div>
       <div>
@@ -24,4 +27,3 @@ const PreferenceInput = (props) => {
 export default reduxForm({
   form: 'prefInput'  // a unique identifier for this form
 })(PreferenceInput)
-
--- client/components/LoginPage.jsx
+++ client/components/LoginPage.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import FacebookLogin from '../actions/action_authentication'
+import LoginForm from './LoginForm.jsx'
 
 const login = () => {
-  return <a className="btn btn-primary"  href="/auth/facebook">Log in with Facebook</a>
+  return (
+    <div className='login-box col-md-4 col-md-offset-4'>
+      <h2>Welcome to the Club.</h2>
+      <a className="btn btn-primary"  href="/auth/facebook">Login with Facebook</a>
+      <LoginForm />
+    </div>
+  )
 }
 
 export default login;
