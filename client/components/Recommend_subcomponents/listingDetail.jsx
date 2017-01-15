ebf93c5a328203506bef563361cb574c8e179614
zeninspire <zeninspire@gmail.com>
2017-01-14 19:53:40 -0800

    [MODIFY](Client): Updated venue information formatting. {AZ}

--- client/components/Recommend_subcomponents/listingDetail.jsx
+++ client/components/Recommend_subcomponents/listingDetail.jsx
@@ -4,21 +4,21 @@ const listingDetail = ({phone, address, yelpCategory, open, dollar, yelpRating, 
   return (
     <div>
       <span className='listingDetail'>Type: {yelpCategory || 'Loading...'}</span> <br/><br/>
+      <span className='listingDetail listingDetail-address'>Address: {address || 'Loading...'}</span><br/><br/>
 
 <div className='listingDetail-center'>
       <ul className='listingDetail-left'>
-        <li className='listingDetail'>Yelp Rating: {yelpRating ? yelpRating+'/5' : 'Loading...'}</li>
+        <li className='listingDetail'>Yelp Rating: {yelpRating ? yelpRating+'/5' : 'Loading...'}</li><br/>
         <li className='listingDetail'>Open now: {open}</li>
       </ul>
 
 
       <ul className='listingDetail-right'>
-        <li className='listingDetail'>Phone: {phone || 'Loading...'}</li>
+        <li className='listingDetail'>Phone: {phone || 'Loading...'}</li><br/>
         <li className='listingDetail'>Price Level: {dollar}</li>
       </ul>
 </div><br/>
 
-      <span className='listingDetail listingDetail-address'>Address: {address || 'Loading...'}</span>
     </div>
   )
 }
